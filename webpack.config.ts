aa911dd5a7436a206f5ad0bfbbcbd07a3640fa7b
slim <vlad.dryg@mail.ru>
2025-01-13 15:07:00 +0200

    :tada: initialization project

---
+++ webpack.config.ts
@@ -0,0 +1,25 @@
+import path from 'path'
+import {Configuration} from 'webpack'
+
+const config: Configuration = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  mode: 'production',
+}
+
+export default config

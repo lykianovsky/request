7557fb8374014b158db2e773c1c074ab6e01ec03
slim <vlad.dryg@mail.ru>
2025-01-14 17:28:44 +0200

    fix: Fixed path to dist/module.js

--- vite.config.ts
+++ vite.config.ts
@@ -15,22 +15,12 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'), // Основной файл библиотеки
-      name: 'MyLibrary', // Имя глобальной переменной, если используете UMD или IIFE формат
-      fileName: (format) => `my-library.${format}.js`, // Название файлов в разных форматах
+      name: 'requestify-lite', // Имя глобальной переменной, если используете UMD или IIFE формат
+      fileName: 'requestify-lite', // Название файлов в разных форматах
     },
     target: 'esnext',
     outDir: 'dist', // Папка для выходных файлов
     emptyOutDir: true, // Очищаем папку dist перед сборкой
-    rollupOptions: {
-      // Параметры Rollup для настройки вывода
-      output: {
-        // Формат модулей
-        format: 'es', // ESModules для библиотеки
-        entryFileNames: '[name].[hash].js', // Уникальные имена для файлов
-        chunkFileNames: '[name].[hash].js', // Уникальные имена для чанков
-        assetFileNames: '[name].[hash][extname]', // Для ассетов (картинок, стилей и т.д.)
-      },
-    },
   },
   plugins: [
     dtsPlugin({

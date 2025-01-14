5d312c99707df0389a30d94597a4ee29c5cd0f0f
slim <vlad.dryg@mail.ru>
2025-01-14 15:30:47 +0200

    feat: Add test for successful and error request

---
+++ src/rest/request/request.test.ts
@@ -0,0 +1,46 @@
+import {request} from '@rest/request/index'
+
+global.fetch = jest.fn()
+
+const MOCK_URL = new URL('https://example.com/api')
+
+describe('request', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('handles successful response', async () => {
+    const mockBody = {data: 'success'}
+
+    const mockResponse = new Response(JSON.stringify(mockBody), {
+      status: 200,
+      headers: {'Content-Type': 'application/json'},
+    })
+
+    ;(fetch as jest.Mock).mockResolvedValueOnce(mockResponse)
+
+    const [response, result] = await request({
+      url: MOCK_URL,
+      options: {method: 'GET'},
+    })
+
+    expect(fetch).toHaveBeenCalledWith(MOCK_URL, {
+      method: 'GET',
+    })
+    expect(response.ok).toBe(true) // Проверяем ключевые свойства ответа
+    expect(result).toEqual(mockBody) // Проверяем данные
+  })
+
+  it('handles error response', async () => {
+    const mockResponse = new Response(null, {status: 404})
+
+    ;(fetch as jest.Mock).mockResolvedValueOnce(mockResponse)
+
+    await expect(
+      request({
+        url: MOCK_URL,
+        options: {method: 'GET'},
+      }),
+    ).rejects.toEqual([mockResponse, undefined])
+  })
+})

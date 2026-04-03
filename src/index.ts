1aa902f00e7b0aeaec6aac17158e99b7093f1cfc
Andrey Novichenko <60508206+anovichenk0@users.noreply.github.com>
2026-04-03 13:28:09 +0200

    feat: Add export exchanges types in main index file

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {request} from '@rest/request'
 export {HttpStatusCode} from '@internal-types/enum/http-status-code'
+export type {ResponseExchange} from '@rest/request/exchanges/response'
+export type {RequestExchange} from '@rest/request/exchanges/request'
